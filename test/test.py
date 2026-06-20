e735db298228d07cfd63c867151d0501a791ece0
Kai Kumano <24wm4308@student.gs.chiba-u.jp>
2026-06-20 15:50:56 +0900

    test2

--- test/test.py
+++ test/test.py
@@ -1,1 +1,9 @@
-test.py
+class Dog:
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+    def bark(self):
+        print(f"{self.name} wow")
+
+my_dog=Dog("pochi", 3)
+my_dog.bark()
